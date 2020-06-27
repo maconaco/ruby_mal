0fefb387020c1bd16e6ade3f3d92def7f6657f70
macololidoll <makomakopg@gmail.com>
2020-06-28 02:07:50 +0900

    feat: add parse_expr,parse_num && Parser Class

--- main.rb
+++ main.rb
@@ -1,16 +1,36 @@
 require "./tokenizer"
 require "./parser"
 
-tokenizer = Tokenizer.new("(+ 10 22 (+ 1 2) 3)", 0)
+#tokenizer = Tokenizer.new("(+ 10 22 (+ 1 2) 3)", 0)
+tokenizer = Tokenizer.new("(+ 1 2 (+ 3 4))", 0,)
 puts tokenizer.call
 p "tokenize --------------------------"
 tokens = p tokenizer.tokenize
 
 index = 0
 
+# インスタンス化する時に空配列わたすの、だいたい頭の悪いパターンなのでやめとく
 parser = Parser.new(0,tokens)
 
 p "parse -----------------------------"
-p parser.curr
 p parser.parse
+
+=begin
+[
+  {:type=>:Exprr,
+    :val=>{:type=>:OPERATOR, :val=>"+"},
+      :children=>[
+        {:type=>:NumberNode,:val=>{:type=>:NUMBER, :val=>3}},
+        {:type=>:NumberNode, :val=>{:type=>:NUMBER, :val=>4}}
+      ]
+    },
+  {:type=>:Exprr,
+    :val=>{:type=>:OPERATOR, :val=>"+"},
+      :children=>[
+        {:type=>:NumberNode, :val=>{:type=>:NUMBER, :val=>1}},
+        {:type=>:NumberNode, :val=>{:type=>:NUMBER, :val=>2}}
+      ]
+  }
+]
+=end
 p "node -----------------------------"
--- parser.rb
+++ parser.rb
@@ -1,11 +1,14 @@
 require "./tokenizer"
 
 class Parser
-  attr_accessor :index, :tokens
+  attr_accessor :index, :tokens, :node
+
   def initialize(index, tokens)
     @index = index
     @tokens = tokens
+    @node = []
   end
+
   def node_type
     @index = 0
     case @tokens[@index][:type]
@@ -16,17 +19,46 @@ class Parser
       else :Unknown
     end
   end
+
   def curr
-    @index < @tokens.length - @index ? @tokens[@index+1] : ''
+    @tokens[@index]
   end
+
   def parse_expr
-  end
-  def parse
-    if @tokens[@index][:type] == :LPAREN
-      return {type: :Exprr, val: curr}
+    op = curr
+    @index+=1
+
+    nums = []
+    while @tokens[@index][:type] == :NUMBER do
+      nums.push(parse_num)
     end
-    return {type: :NumberNode, val: @tokens[@index]}
-    if @tokens[@index][:type] == [:RPAREN]
+    parse
+    p "#{parse}"
+    {type: :Exprr, val: op, children: nums}
+  end
+
+  def parse_num
+    tok = curr
+    return nil if tok.nil?
+    if tok[:type] == :LPAREN
+      parse
+    else
+      @index+=1
+      {type: :NumberNode, val: tok}
+    end
+  end
+
+  def parse
+    c = @tokens[@index]
+    if c[:type] == :LPAREN then
+      @index+=1
+      @node.push parse_expr
+    elsif c[:type] == :NUMBER then
+      @node.push parse_num
+    elsif c[:type] == :RPAREN then
+        "RPARENです"
+    else
+      "error #{c}"
     end
   end
 
